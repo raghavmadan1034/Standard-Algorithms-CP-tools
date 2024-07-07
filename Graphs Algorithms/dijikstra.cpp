1b82b268c6e2285676e06d9f284311242b3aeb97
Raghav Madan <128486510+raghavmadan1034@users.noreply.github.com>
2024-07-07 18:07:13 +0530

    Update dijikstra.cpp

--- Graphs Algorithms/dijikstra.cpp
+++ Graphs Algorithms/dijikstra.cpp
@@ -1,48 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-//dijkstra's Algorithm uses Priority Queues (min heap)
-//We will store negative elements (distances) in priority queue coz C++ PQ is max heap so minimum abs distance is on the top, (maximum element among all)
-void dijkstra(vector<vector<pair<ll,ll>>>&adjlist,vector<ll>&distance,ll source,vector<ll>&processed){
-    distance[source]=0;
-    priority_queue<pair<ll,ll>>pq;
-    pq.push({0,source});
+#define ll long long 
+ll INF=1e10;
+void bfs(vector<vector<pair<ll,ll>>>&adjlist,vector<ll>&dist,ll start,vector<ll>&processed){
+    priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>>pq;
+    pq.push({0,start});
+    dist[start]=0;
     while(pq.size()){
-        ll a=pq.top().second;
+        ll curr=pq.top().second;
+        ll d=pq.top().first;
         pq.pop();
-        if(processed[a])continue; //->This is important otherwise chance of TLE(if initially a is not processed and before calling 'a' it gets processed)
-        processed[a]=1;
-        for(auto y:adjlist[a]){
-            ll b=y.first;
-            ll w=y.second;
-            if(processed[b]==0 && distance[a]+w<distance[b]){
-                distance[b]=distance[a]+w;
-                pq.push({-distance[b],b});
-            } 
-            //if distance[a]+w>distance[b] => this implies that the node b has already been selected
-            //Property of Dijkstra :-Once a node has been selected,its distance is FINAL.
-            //so if distance[a]+w>distance[b] =>you found its min distace already=> no need of pushing it back...
+        if(processed[curr])continue;
+        processed[curr]=1;
+        for(auto x:adjlist[curr]){
+            if(!processed[x.first] && d+x.second<dist[x.first]){
+                dist[x.first]=d+x.second;
+                pq.push({dist[x.first],x.first});
+            }
         }
     }
 }
 int main(){
-    //The graph is stored as adjlists so that adj[a] contains pair (b,w) always when there is edge from a to b with weight W
-    ll n,m;
-    cin>>n>>m;
-    vector<vector<pair<ll,ll>>>adjlist(n+1);//1 based indexing of nodes
+    ll n,m;cin>>n>>m;
+    vector<vector<pair<ll,ll>>>adjlist(n+1);
     for(ll i=0;i<m;i++){
-        ll a,b,w;
-        cin>>a>>b>>w;
+        ll a,b,w;cin>>a>>b>>w;
         adjlist[a].push_back({b,w});
-        adjlist[b].push_back({a,w});//DONT WRITE THIS LINE IF GRAPH IS DIRECTED!!!!
+        adjlist[b].push_back({a,w});//for directed graph comment out this line
     }
-    vector<ll>distance(n+1,LLONG_MAX);
-    vector<ll>processed(n+1,0);
-    //There is difference between visited of BFS and processed of dijsktra
-    //in dijkstra you can push same node multiple times in pq,but process vo ek hi baar hogi
-    dijkstra(adjlist,distance,1,processed);
-    for(ll i=1;i<=n;i++){
-        cout<<distance[i]<<" ";
-    }
+    vector<ll>dist(n+1,INF);vector<ll>processed(n+1);
+    bfs(adjlist,dist,1,processed);
+    for(ll i=1;i<=n;i++)cout<<dist[i]<<" ";
 
 }
